1af935f4467c6abba36c3497547ae521dca6ee51
fabianoairon <fabianoairon@gmail.com>
2022-11-07 06:19:41 -0300

    blinking tilemap

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -6,14 +6,22 @@ public class GameUI : MonoBehaviour
     public GameObject GameOverMessage;
     public AudioSource AudioSource;
     public AudioClip BeginningMusic;
+    public BlinkTilemapColor BlinkTilemapColor;
     private GameManager _gameManager;
     void Start()
     {
         _gameManager = FindObjectOfType<GameManager>();
         _gameManager.OnGameStarting += _gameManager_OnGameStarting;
         _gameManager.OnGameOver += _gameManager_OnGameOver;
+        GameManager.OnVictory += GameManager_OnVictory;
         AudioSource.PlayOneShot(BeginningMusic);
         GameOverMessage.SetActive(false);
+    }
+
+    private void GameManager_OnVictory()
+    {
+        if (BlinkTilemapColor == null) return;
+        BlinkTilemapColor.enabled = true;
     }
 
     private void _gameManager_OnGameOver()
